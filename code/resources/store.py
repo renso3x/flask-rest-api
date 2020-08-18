58b97ff5079337e5734c6ac8e5c0f3b7d442da06
renso3x <enso123456@gmail.com>
2020-08-18 18:50:54 +0800

    first commit

---
+++ code/resources/store.py
@@ -0,0 +1,36 @@
+from flask_restful import Resource
+from models.store import StoreModel
+
+
+class Store(Resource):
+    def get(self, name):
+        store = StoreModel.find_by_name(name)
+        if store:
+            return store.json()
+
+        return {'message': 'Store not found'}, 404
+
+    def post(self, name):
+        if StoreModel.find_by_name(name):
+            return {'message': f'An store with {name} already exists'}, 400
+
+        store = StoreModel(name)
+        try:
+            store.save_to_db()
+        except:
+            return {'message': 'An error occured on saving'}, 500
+
+        return store.json(), 201
+
+    def delete(self, name):
+        store = StoreModel.find_by_name(name)
+
+        if store:
+            store.delete_from_db()
+
+        return {'message': 'Store deleted'}, 200
+
+
+class StoreList(Resource):
+    def get(self):
+        return {'stores': [store.json() for store in StoreModel.query.all()]}
